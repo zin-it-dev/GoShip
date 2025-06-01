9d9ea6be61e1d50e1d83119a2c8db594310fdf67
Lê Hiển Vinh <zin.it.dev@gmail.com>
2025-06-01 15:10:54 +0700

    🎊 Log & Environment Variable 🎊
    🧧 Use .env file
    🚀 Log server URL

--- server/main.go
+++ server/main.go
@@ -1,9 +1,12 @@
 package main
 
 import (
+	"log"
 	"net/http"
+	"os"
 
 	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
 )
 
 type Post struct {
@@ -22,7 +25,31 @@ func getPosts(c *gin.Context) {
 	c.IndentedJSON(http.StatusOK, posts)
 }
 
+func getPost(c *gin.Context) {
+	id := c.Param("id")
+
+	for _, p := range posts {
+		if p.ID == id {
+			c.IndentedJSON(http.StatusOK, p)
+			return
+		}
+	}
+
+	c.IndentedJSON(http.StatusNotFound, gin.H{"message": "Post not found"})
+}
+
 func main() {
+	err := godotenv.Load()
+  	if err != nil {
+    	log.Fatal("Error loading .env file")
+  	}
+
+	port := os.Getenv("PORT")
+
+	if port == "" {
+		port = "8080"
+	}
+
 	router := gin.Default()
 
 	router.GET("/", func(c *gin.Context)  {
@@ -32,6 +59,8 @@ func main() {
 	})
 
 	router.GET("/posts", getPosts)
+	router.GET("/posts/:id", getPost)
 
-	router.Run("localhost:8080")
+	log.Printf("🚀 Server is running at http://localhost:%s", port)
+	router.Run(":" + port)
 }
